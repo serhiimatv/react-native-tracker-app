351e4cefbe5ff9733983af99f09b760c64b004ff
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-20 00:35:14 +0200

    add validation

--- components/ManageExpense/ExpenseForm.tsx
+++ components/ManageExpense/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 import Input from './Input';
 import { useState } from 'react';
 
@@ -47,6 +47,17 @@ const ExpenseForm = ({
       date: new Date(inputValues.date),
       description: inputValues.description,
     } satisfies NewExpense;
+
+    const isAmountValid =
+      !Number.isNaN(expenseData.amount) && expenseData.amount > 0;
+    const isDateValid = !Number.isNaN(new Date(expenseData.date).getTime());
+    const isDescriptionValid = expenseData.description.trim().length > 0;
+
+    if (!isAmountValid || !isDateValid || !isDescriptionValid) {
+      Alert.alert('Invalid input', 'Please check your input values');
+      return;
+    }
+
     onSubmit(expenseData);
   };
 
